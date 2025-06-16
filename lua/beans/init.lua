42e1d0f5a1fd69f5eec15cc765748924287ad1f5
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-06-16 20:06:16 +0100

    add nvim-ts-autotag to automatically complete html element tags

--- lua/beans/init.lua
+++ lua/beans/init.lua
@@ -1,16 +1,16 @@
-require "beans.remap"
-require "beans.options"
+require("beans.remap")
+require("beans.options")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,4 +22,3 @@ require("lazy").setup("plugins")
 vim.cmd("colorscheme catppuccin")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
