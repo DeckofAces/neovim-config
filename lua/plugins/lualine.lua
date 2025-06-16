fa6bb91f4be8532ee0774ad87a29e8d0300b26ff
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-06-16 20:28:20 +0100

    update lualine to show current file relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,33 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-      theme = 'dracula'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"SmiteshP/nvim-navic",
+	},
+	config = function()
+		local navic = require("nvim-navic")
+		local on_attach = function(client, bufnr)
+			if client.server_capabilities.documentSymbolProvider then
+				navic.attach(client, bufnr)
+			end
+		end
+
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+					{
+						navic.get_location,
+						cond = navic.is_available,
+					},
+				},
+			},
+		})
+	end,
 }
