29bb5748151beb957ddcc4e3bef5bafcfe1a4ab0
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-10-18 09:36:20 +0100

    update blink config, formatter config, install indent-line, install todo-comments, install tailwind-tools

--- lua/beans/options.lua
+++ lua/beans/options.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = "a"
@@ -32,3 +32,4 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.termguicolors = true
 vim.o.updatetime = 150
+vim.o.confirm = true
--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -72,46 +72,7 @@ return {
 			completion = {
 				-- By default, you may press `<c-space>` to show the documentation.
 				-- Optionally, set `auto_show = true` to show the documentation after a delay.
-				documentation = { auto_show = true, auto_show_delay_ms = 500 },
-				menu = {
-					draw = {
-						components = {
-							-- customize the drawing of kind icons
-							kind_icon = {
-								text = function(ctx)
-									-- default kind icon
-									local icon = ctx.kind_icon
-									-- if LSP source, check for color derived from documentation
-									if ctx.item.source_name == "LSP" then
-										local color_item = require("nvim-highlight-colors").format(
-											ctx.item.documentation,
-											{ kind = ctx.kind }
-										)
-										if color_item and color_item.abbr ~= "" then
-											icon = color_item.abbr
-										end
-									end
-									return icon .. ctx.icon_gap
-								end,
-								highlight = function(ctx)
-									-- default highlight group
-									local highlight = "BlinkCmpKind" .. ctx.kind
-									-- if LSP source, check for color derived from documentation
-									if ctx.item.source_name == "LSP" then
-										local color_item = require("nvim-highlight-colors").format(
-											ctx.item.documentation,
-											{ kind = ctx.kind }
-										)
-										if color_item and color_item.abbr_hl_group then
-											highlight = color_item.abbr_hl_group
-										end
-									end
-									return highlight
-								end,
-							},
-						},
-					},
-				},
+				documentation = { auto_show = false, auto_show_delay_ms = 500 },
 			},
 
 			sources = {
@@ -130,7 +91,7 @@ return {
 			-- the rust implementation via `'prefer_rust_with_warning'`
 			--
 			-- See :h blink-cmp-config-fuzzy for more information
-			fuzzy = { implementation = "prefer_rust_with_warning" },
+			fuzzy = { implementation = "lua" },
 
 			-- Shows a signature help window while you type arguments for a function
 			signature = { enabled = true },
