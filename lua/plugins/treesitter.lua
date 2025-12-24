847d2f76895cc6b42938b66466121b911cedc37c
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-12-24 22:14:18 +0100

    update treesitter.lua
    previous plugin installation syntax is broken in newer versions of
    neovim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,23 @@
-return { 
-  "nvim-treesitter/nvim-treesitter",
-  build= ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript", "typescript", "html", "css", "vim"},
-      -- Auto install languages that are not installed
-      auto_install = true,
-      highlight = { 
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = { enable = true }
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+
+	opts = {
+		ensure_installed = {
+			"lua",
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+			"vim",
+			"vimdoc",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
 }
